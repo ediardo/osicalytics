660b628d87892b73a3af7853572a5e2cf2dcb001
Eddie Ramirez <ediardo@Eddies-MacBook-Pro.local>
2016-05-05 10:56:26 -0500

    Added dev web server in packages.json and a new grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,15 @@ module.exports = function(grunt) {
         }
       }
     },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: "localhost",
+          open: true
+        }
+      }
+    },
     watch: {
       css: {
         files: '**/*.scss',
@@ -20,5 +29,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', ['connect', 'watch']);
 }
